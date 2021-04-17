1ed1559badef5aaca05ce41dc27a0381c9dc5cc5
HQalphaca <meddugi7170@gmail.com>
2021-04-18 02:08:30 +0900

    no message

---
+++ Assets/MoveUI.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public void sss()
+    {
+
+        float Speed = 10;
+
+
+    }
+}
